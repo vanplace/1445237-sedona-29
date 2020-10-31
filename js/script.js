752edd3e359452bb94d1a860c253c3d713067c0d
vanplace <70973002+vanplace@users.noreply.github.com>
2020-10-31 16:58:35 +0300

    поправил через PP, добавил JS

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const ModalToOpen = document.querySelector(".modal-to-open");
+const Modal = document.querySelector (".modal");
+const ModalToClosen = document.querySelector(".modal-search");
+
+ModalToClosen.addEventListener("click", function(evt){
+    evt.preventDefault();
+    Modal.classList.add("modal-none");
+});
+
+ModalToOpen.addEventListener("click", function(evt){
+    evt.preventDefault();
+    Modal.classList.remove("modal-none");
+})
